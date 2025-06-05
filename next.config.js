200670e8065d115174ee3e460ff30fd242c4d401
Khizar-cyber-dev <hafizkhizarkhizar07@gmail.com>
2025-06-05 10:07:42 +0500

    solv the config error

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // 'appDir' is not needed anymore in Next.js 13+
+    // Remove it unless you're on an older version
+  },
+  // 'swcMinify' is no longer needed; it's default in Next 13+
+}
+
+module.exports = nextConfig;
