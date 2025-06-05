ab646593ae5600e0c954ee61c5c106abde2075ee
Khizar-cyber-dev <hafizkhizarkhizar07@gmail.com>
2025-06-05 10:16:17 +0500

    make premiem error sovle

--- premium/page.tsx
+++ premium/page.tsx
@@ -1,13 +1,12 @@
-import prisma from "@/db/prisma";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { prisma } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-	const { getUser } = getKindeServerSession();
-	const user = await getUser();
-	if (!user) return redirect("/");
+	const { userId } = await auth();
+	if (!userId) return redirect("/");
 
-	const userProfile = await prisma.user.findUnique({ where: { id: user.id } });
+	const userProfile = await prisma.user.findUnique({ where: { clerkId: userId } });
 	if (userProfile?.plan === "free") return redirect("/");
 
 	return <div className='max-w-7xl mx-auto'>You are on the premium plan so you can see this page</div>;
